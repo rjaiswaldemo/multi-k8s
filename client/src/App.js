f440d34b50adc0e11947a8153ab55a9265209230
Rakesh Jaiswal <inforakeshjaiswal@gmail.com>
2023-10-01 15:44:26 +0530

    changes to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <header className="App-header">
+		<h1 className="App-title">Fibonacci Calculator</h1>
           <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
